4579a627e386d7e2e30abe41667ace3f610d0722
bishowrajpandey-ctrl <bishowrajpandey@gmail.com>
2026-04-05 19:13:12 +1000

    Initialize Flask app with debug mode on
    Added a basic Flask application with debug mode enabled.

--- Week5/Vulnerable_Setup/vulnerable_app.py
+++ Week5/Vulnerable_Setup/vulnerable_app.py
@@ -1,1 +1,13 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+# MISCONFIGURATION: Debug mode ON
+app.config['DEBUG'] = True
+
+@app.route('/')
+def home():
+    return "Vulnerable Application Running"
+
+if __name__ == '__main__':
+    app.run()
